45adbc2e630a7dc6630c2d1f5aac200d6bb8a7a0
Fernando Ricardo Strijeski <fernando.strijeski@linx.com.br>
2024-04-29 17:17:25 -0300

    Solução criada com 2 projetos, sendo o primeiro para enviar qualquer mensagem e o segundo para capturar a mensagem enviada no mesmo canal. A mensagem no exemplo é uma classe serializada em json.

---
+++ EnviaDados/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "classeAluno", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+Console.WriteLine("Digite seu nome aqui:");
+
+while (true)
+{
+    string message = Console.ReadLine();
+
+    if (message == "")
+        break;
+
+    var aluno = new Aluno() {Id = 1, Nome = message };
+    message = JsonSerializer.Serialize(aluno);
+
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange: string.Empty, routingKey: "classeAluno", basicProperties: null, body: body);
+    Console.WriteLine($" [x] Enviado {message}");
+
+}
+
+class Aluno()
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+
+}
---
+++ RecebeDados/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Text.Json;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "classeAluno", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+Console.WriteLine(" [*] Aguardando mensagens.");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+
+    var aluno = JsonSerializer.Deserialize<Aluno>(message);
+
+    Console.WriteLine($" [x] Recebido: {aluno.Id} - {aluno.Nome}");
+};
+
+channel.BasicConsume(queue: "classeAluno", autoAck: true, consumer: consumer);
+
+Console.WriteLine(" Aperte enter para sair.");
+Console.ReadLine();
+
+class Aluno()
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+
+}
